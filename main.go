68bcd6b42bbda80666bff8beb7e38db010bcca9c
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-06 11:16:45 +0530

    added main go file

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lordlabuckdas/gtfoo/gtfobins"
+	"github.com/lordlabuckdas/gtfoo/lolbas"
+)
+
+func main() {
+	fmt.Println(gtfobins.Greet("GTFObins"))
+	fmt.Println(lolbas.Hello("LOLBAS"))
+}
