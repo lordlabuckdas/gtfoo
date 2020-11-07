481fe78bb7e8832562cc41ce0dcd9904d56153d4
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-07 22:14:26 +0530

    added colors for lolbas

--- lolbas/lolbas.go
+++ lolbas/lolbas.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorWhite  = "\033[37m"
+)
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v", name)
+	return fmt.Sprintf("%sHello, %v%s", colorRed, name, colorReset)
 }
