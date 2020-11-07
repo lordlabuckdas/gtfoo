7c53e0528bc1741d7addbf9caf545982e4b85220
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-07 22:14:15 +0530

    added colors for gtfobins

--- gtfobins/gtfobins.go
+++ gtfobins/gtfobins.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorWhite  = "\033[37m"
+)
+
 func Greet(name string) string {
-	return fmt.Sprintf("Hi, %v", name)
+	return fmt.Sprintf("%sHi, %v%s", colorBlue, name, colorReset)
 }
