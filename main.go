18517f804e65f1a86c8bedc8bd91e70ff06cdd4f
krishna <krishnan_1901ee50@iitp.ac.in>
2020-11-07 22:38:52 +0530

    added basic cmdline flag parsing

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/lordlabuckdas/gtfoo/gtfobins"
 	"github.com/lordlabuckdas/gtfoo/lolbas"
 )
 
+var gtfoSearch, lolbasSearch *string
+
+func init() {
+	gtfoSearch = flag.String("g", "", "enter binary to be searched in gtfobins")
+	lolbasSearch = flag.String("l", "", "enter binary to be searched in lolbas")
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println(gtfobins.Greet("GTFObins"))
-	fmt.Println(lolbas.Hello("LOLBAS"))
+	fmt.Println(gtfobins.Greet(*gtfoSearch))
+	fmt.Println(lolbas.Hello(*lolbasSearch))
 }
