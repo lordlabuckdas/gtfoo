9932da1d2281270f6258e2d308623bcd1c4b9e48
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-12 11:40:10 +0530

    updated gtfobins w/ logging

--- gtfobins/go.mod
+++ gtfobins/go.mod
@@ -2,9 +2,4 @@ module github.com/lordlabuckdas/gtfoo/gtfobins
 
 go 1.15
 
-replace github.com/lordlabuckdas/gtfoo/utils => ../utils
-
-require (
-	github.com/lordlabuckdas/gtfoo/utils v0.0.0-00010101000000-000000000000
-	gopkg.in/yaml.v2 v2.3.0
-)
+require gopkg.in/yaml.v2 v2.3.0
--- gtfobins/gtfobins.go
+++ gtfobins/gtfobins.go
@@ -3,36 +3,52 @@ package gtfobins
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/lordlabuckdas/gtfoo/utils"
 	"gopkg.in/yaml.v2"
 )
+
+func init() {
+	logFileName := "logs.txt"
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Printf("Could not create log file: %s", err)
+	}
+
+	infoLogger = log.New(logFile, colorYellow+"[INFO] "+colorReset, log.Ldate|log.Ltime|log.Lshortfile)
+	errorLogger = log.New(logFile, colorRed+"[ERROR] "+colorReset, log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func getExploit(name string) gtfoStruct {
 	url := fmt.Sprintf(baseURL, name)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("Error fetching data : %s\n", err)
+		errorLogger.Fatalf("Error fetching data : %s\n", err)
+	} else {
+		infoLogger.Printf("Fetching data from %s", url)
 	}
+
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error reading data : %s\n", err)
+		errorLogger.Fatalf("Error reading data : %s\n", err)
+	} else {
+		infoLogger.Printf("Reading data from GTFObins for %s", name)
 	}
 
 	var gtfoExploit gtfoStruct
 	err = yaml.Unmarshal(body, &gtfoExploit)
 	if err != nil {
-		fmt.Printf("Error parsing YAML file: %s\n", err)
+		errorLogger.Fatalf("Error parsing YAML file: %s\n", err)
+	} else {
+		infoLogger.Printf("Parsing %s data", name)
 	}
-	return gtfoExploit
-}
 
-func Greet(name string) string {
-	return fmt.Sprintf("%sHi, %v%s", utils.ColorBlue, name, utils.ColorReset)
+	return gtfoExploit
 }
 
 func GtfobinMain(name string) {
